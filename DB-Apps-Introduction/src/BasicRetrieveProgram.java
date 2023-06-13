67658d0d86139efabc883901ccd2d7da1ad1ce7d
Давид Кръстев <106105745+davidchrastev@users.noreply.github.com>
2023-06-13 21:10:59 +0300

    My own retrieve program

--- DB-Apps-Introduction/src/BasicRetrieveProgram.java
+++ DB-Apps-Introduction/src/BasicRetrieveProgram.java
@@ -3,31 +3,58 @@ import java.util.Properties;
 import java.util.Scanner;
 
 public class BasicRetrieveProgram {
+    private static final String SELECT_USER_GAMES_COUNT_BY_USERNAME =
+            "SELECT first_name, last_name, COUNT(ug.user_id) as count " +
+            "FROM users u JOIN users_games ug on u.id = ug.user_id " +
+            "WHERE user_name = ? " +
+            "GROUP BY first_name, last_name";
     public static void main(String[] args) throws SQLException {
-        Scanner scanner = new Scanner(System.in);
+        start();
+    }
 
+    private static void start() throws SQLException {
+        Scanner scanner = new Scanner(System.in);
+        String user_name = scanner.nextLine();
+
+        Connection connection = setUpConnection();
+
+        PreparedStatement pr = getPreparedStatement(user_name, connection);
+
+        ResultSet rs = pr.executeQuery();
+
+        String response = getResponse(user_name, rs);
+
+        System.out.println(response);
+    }
+
+    private static PreparedStatement getPreparedStatement(String user_name, Connection connection) throws SQLException {
+        PreparedStatement pr = connection.prepareStatement(SELECT_USER_GAMES_COUNT_BY_USERNAME);
+
+        pr.setString(1, user_name);
+        return pr;
+    }
+
+    private static Connection setUpConnection() throws SQLException {
         Properties properties = new Properties();
         properties.setProperty("user", "root");
         properties.setProperty("password", "1234");
 
-        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/diablo", properties);
+        return DriverManager.getConnection("jdbc:mysql://localhost:3306/diablo", properties);
+    }
 
-        String user_name = scanner.nextLine();
-
-        PreparedStatement pr = connection.prepareStatement("SELECT first_name, last_name, COUNT(ug.user_id) as count FROM users u JOIN users_games ug on u.id = ug.user_id WHERE user_name = ? GROUP BY first_name, last_name");
-        pr.setString(1, user_name);
-
-        ResultSet rs = pr.executeQuery();
+    private static String getResponse(String user_name, ResultSet rs) throws SQLException {
+        StringBuilder response = new StringBuilder();
 
         if (rs.next()) {
             int count = rs.getInt("count");
             String firstName = rs.getString("first_name");
             String lastName = rs.getString("last_name");
-            System.out.println("User: " + user_name);
-            System.out.println(firstName + " " + lastName + " has played " + count + " games");
+            response.append("User: ").append(user_name).append(System.lineSeparator());
+            response.append(firstName).append(" ").append(lastName).append(" has played ").append(count).append(" games");
         } else {
-            System.out.println("No such user exists");
+            response.append("No such user exists");
         }
 
+        return response.toString();
     }
 }
