7bcb15ba7b9ef3ec612554e61d3d66488220155c
Давид Кръстев <106105745+davidchrastev@users.noreply.github.com>
2023-06-20 15:19:08 +0300

    Refactoring code

--- DB-Apps-Introduction-Exercise/DB-Apps-Introduction-Exercise/src/N06_remove_villain/VillainRemover.java
+++ DB-Apps-Introduction-Exercise/DB-Apps-Introduction-Exercise/src/N06_remove_villain/VillainRemover.java
@@ -3,8 +3,6 @@ package N06_remove_villain;
 import N04_add_minions.ConnectionSQL;
 
 import java.sql.*;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 
 public class VillainRemover {
@@ -13,19 +11,21 @@ public class VillainRemover {
 
         int villainId = readVillainID();
 
-        String result = deleteVillainAndHisMinions(villainId);
+        String villainAndCountMinions = deleteVillainAndMinions(villainId);
 
-        printResult(result);
+        printResult(villainAndCountMinions);
     }
 
-    private static void printResult(String villainName) {
-        if (villainName == null) {
-            System.out.println("No such villain was found.");
+    private static void printResult(String villainAndCountMinions) {
+        if (villainAndCountMinions == null) {
+            System.out.println(VillainRemoverPrintConstants.NO_VILLAIN_FOUND);
         } else {
-            List<String> result = Arrays.stream(villainName.split(" ")).toList();
+            String[] parts = villainAndCountMinions.split(" ");
+            String villainName = parts[0];
+            String countMinions = parts[1];
 
-            System.out.println(result.get(0) + " was deleted");
-            System.out.println(result.get(1) + " minions released");
+            System.out.printf(VillainRemoverPrintConstants.VILLAIN_DELETED, villainName);
+            System.out.printf(VillainRemoverPrintConstants.COUNT_MINIONS_DELETED, countMinions);
         }
     }
 
@@ -34,9 +34,9 @@ public class VillainRemover {
         return Integer.parseInt(scanner.nextLine());
     }
 
-    public static String deleteVillainAndHisMinions(int villainId) {
+    public static String deleteVillainAndMinions(int villainId) {
         String villainName = null;
-        int releasedMinions = 0;
+        int numMinionsReleased = 0;
 
         try (Connection connection = ConnectionSQL.setUpConnection()) {
             PreparedStatement selectStatement = connection.prepareStatement(MinionStatements.SELECT_VILLAIN_BY_ID);
@@ -45,20 +45,19 @@ public class VillainRemover {
 
             if (resultSet.next()) {
                 villainName = resultSet.getString("name");
-
-                releasedMinions = releaseMinions(villainId);
+                numMinionsReleased = releaseMinions(villainId);
 
                 PreparedStatement deleteVillainStatement = connection.prepareStatement(MinionStatements.DELETE_VILLAIN_BY_ID);
                 deleteVillainStatement.setInt(1, villainId);
                 deleteVillainStatement.executeUpdate();
             }
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
 
-        return villainName + " " + releasedMinions;
+        return villainName + " " + numMinionsReleased;
     }
-
 
     public static int releaseMinions(int villainId) {
         int numMinionsReleased = 0;
--- DB-Apps-Introduction-Exercise/DB-Apps-Introduction-Exercise/src/N06_remove_villain/VillainRemoverPrintConstants.java
+++ DB-Apps-Introduction-Exercise/DB-Apps-Introduction-Exercise/src/N06_remove_villain/VillainRemoverPrintConstants.java
@@ -4,4 +4,10 @@ public class VillainRemoverPrintConstants {
 
     public static final String ENTER_VILLAIN_ID = "Enter villain ID";
 
+    public static final String VILLAIN_DELETED = "%s was deleted\n";
+
+    public static final String COUNT_MINIONS_DELETED = "%s minions released\n";
+
+    public static final String NO_VILLAIN_FOUND = "No such villain was found.";
+
 }
